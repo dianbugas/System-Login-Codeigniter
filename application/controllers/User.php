7c1a2dd31079ff5712ee04da3d8a256008411ceb
dianynf <dianbugas@gmail.com>
2019-05-25 11:13:06 +0700

    view user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -7,6 +7,6 @@ class User extends CI_Controller
           {
                     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-                    $this->load->view('user/index');
+                    $this->load->view('user/index', $data);
           }
 }
