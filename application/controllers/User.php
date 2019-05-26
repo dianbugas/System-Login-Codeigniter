607e62a5ebb4135d175a2b0714ee0681ceef67b8
dianynf <dianbugas@gmail.com>
2019-05-26 11:30:36 +0700

    my profile users

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,8 +5,8 @@ class User extends CI_Controller
 {
           public function index()
           {
+                    $data['title'] = 'My Profile';
                     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-
                     $this->load->view('user/index', $data);
           }
 }
