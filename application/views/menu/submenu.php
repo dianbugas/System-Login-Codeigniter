a25747bc524bc21c24b1aecbe8ffca69559bdf43
dianynf <dianbugas@gmail.com>
2019-06-12 20:43:46 +0700

    ----

--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -35,7 +35,7 @@
       <td><?= $sm['is_active']; ?></td>
       <td>
       <a href="#" class="badge badge-success">Edit</a>
-      <a href="#" class="badge badge-danger">delete</a>
+      <a href="<?= base_url(); ?>menu/hapus/<?= $sm['id']; ?>" class="badge badge-danger">delete</a>
       </td>
     </tr>
 <?php endforeach; ?>
