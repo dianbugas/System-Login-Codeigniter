683dd80bfe9e89f0336e0d51fa4b00ee42df9e17
dianynf <dianbugas@gmail.com>
2019-06-13 21:41:59 +0700

    role-access

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -42,11 +42,34 @@ class Admin extends CI_Controller
 
         // intinya ada 2 hal. 1 role id nya berapa kemudian tampilkan semua menu                            
         $data['role'] = $this->db->get_where('user_role', ['id' => $role_id])->row_array();
+
+        $this->db->where('id !=', 1);
         $data['menu'] = $this->db->get('user_menu')->result_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
         $this->load->view('admin/role-access', $data);
         $this->load->view('templates/footer');
+    }
+
+    public function changeAccess(){
+        $menu_id = $this->input->post('menuId');
+        $role_id = $this->input->post('roleId');
+
+        //cek ketika di ceklis maka akan bertamah. ketika di hilangin maka akan terapus
+        $data = [
+            'role_id' => $role_id,
+            'menu_id' => $menu_id
+        ];
+        //query berdasarkan data di atas
+        $result = $this->db->get_where('user_access_menu', $data);
+
+        if($result->num_rows() < 1) {
+            $this->db->insert('user_access_menu', $data);
+        }else{
+            $this->db->delete('user_access_menu', $data);
+        }
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Access Changed!</div>');
     }
 }
--- application/views/admin/role-access.php
+++ application/views/admin/role-access.php
@@ -1,40 +1,41 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-<!-- Page Heading -->
-<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-  <div class="row">
-    <div class="col-lg-6">
-      <?= $this->session->flashdata('message'); ?>
-      <h5>Role : <?= $role['role']; ?></h5>
-    <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Menu</th>
-      <th scope="col">Access</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php $i = 1; ?>
-    <!-- $menu ambil dari controller admin baris 46  -->
-    <?php foreach ($menu as $m) : ?>
-    <tr>
-      <th scope="row"><?= $i++; ?></th>
-      <td><?= $m['menu']; ?></td>
-      <td>
-          <div class="form-check">
-              <!-- check_accces akan menerima dua parameter 1 role idnya berapa ambil dari baris 9 dan menu idnya berapa dari baris ke 21 nenti di query -->
-  <input class="form-check-input" type="checkbox" <?= check_access($role['id'], $m['id']); ?>>
-</div>
-      </td>
-    </tr>
-<?php endforeach; ?>
-  </tbody>
-</table>
-                                        </div>
-
-                    </div>
+  <!-- Page Heading -->
+  <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="row">
+      <div class="col-lg-6">
+        <?= $this->session->flashdata('message'); ?>
+        <h5>Role : <?= $role['role']; ?></h5>
+      <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Menu</th>
+        <th scope="col">Access</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php $i = 1; ?>
+      <!-- $menu ambil dari controller admin baris 46  -->
+      <?php foreach ($menu as $m) : ?>
+      <tr>
+        <th scope="row"><?= $i++; ?></th>
+        <td><?= $m['menu']; ?></td>
+        <td>
+            <div class="form-check">
+              <!-- check_accces akan menerima dua parameter 1 role idnya berapa ambil dari baris 9 dan menu idnya berapa dari baris ke 21 nenti di query // untuk helper-->
+              <input class="form-check-input" type="checkbox" <?= check_access($role['id'], $m['id']); ?> 
+              data-role="<?= $role['id'] ?>" data-menu="<?= $m['id']; ?>">
+              <!-- baris 29 untuk jquery -->
+            </div>
+        </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+    </div>
+  </div>
 </div>
 <!-- /.container-fluid -->
 </div>
