03632afc8af2a0a498a4792551769a26ee661063
dianynf <dianbugas@gmail.com>
2019-06-13 22:18:36 +0700

    role-access

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,22 +3,34 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
-                    public function __construct()
-                    {
-                                        parent::__construct();
-                                        // di tendang supaya user sembarangan tdk masuk sembarangan lewat url
-                                        is_logged_in();
-                    }
+    public function __construct()
+    {
+        parent::__construct();
+        // di tendang supaya user sembarangan tdk masuk sembarangan lewat url
+        is_logged_in();
+    }
 
-                    public function index()
-                    {
-                                        $data['title'] = 'My Profile';
-                                        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    public function index()
+    {
+        $data['title'] = 'My Profile';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-                                        $this->load->view('templates/header', $data);
-                                        $this->load->view('templates/sidebar', $data);
-                                        $this->load->view('templates/topbar', $data);
-                                        $this->load->view('user/index', $data);
-                                        $this->load->view('templates/footer');
-                    }
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function edit()
+    {
+        $data['title'] = 'Edit Profile';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/edit', $data);
+        $this->load->view('templates/footer');
+    }
 }
---
+++ application/views/user/edit.php
@@ -0,0 +1,12 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="row">
+        <div class="col-lg-8"></div>
+    </div>
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- End of Main Content -->
+
