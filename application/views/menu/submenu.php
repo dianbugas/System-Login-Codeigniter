70f4df064001e9e682f197c3b991df078c42cedd
dianynf <dianbugas@gmail.com>
2019-08-17 23:25:57 +0700

    merapihkan

--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -1,46 +1,46 @@
-<!-- Begin Page Content -->
-<div class="container-fluid">
-<!-- Page Heading -->
-<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-  <div class="row">
-    <div class="col-lg">
-      <!-- untuk menampilkan erorr -->
-      <?php if (validation_errors()) : ?>
-        <div class="alert alert-danger" role="alert"></div>
-          <?= validation_errors(); ?>
-      <?php endif; ?>
-      <?= $this->session->flashdata('message'); ?>
-      <a href="" class="btn btn-primary mb-2" data-toggle="modal" data-target="#newSubMenuModal">Add New Submenu</a>
-    <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Title</th>
-      <th scope="col">Menu</th>
-      <th scope="col">Url</th>
-      <th scope="col">Icon</th>
-      <th scope="col">Active</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php $i = 1; ?>
-    <?php foreach ($subMenu as $sm) : ?>
-    <tr>
-      <th scope="row"><?= $i++; ?></th>
-      <td><?= $sm['title']; ?></td>
-      <td><?= $sm['menu']; ?></td>
-      <td><?= $sm['url']; ?></td>
-      <td><?= $sm['icon']; ?></td>
-      <td><?= $sm['is_active']; ?></td>
-      <td>
-      <a href="<?= base_url(); ?>menu/editsub/<?= $sm['id']; ?>" class="badge badge-success">Edit</a>
-      <a href="<?= base_url(); ?>menu/hapus/<?= $sm['id']; ?>" class="badge badge-danger">delete</a>
-      </td>
-    </tr>
-<?php endforeach; ?>
-  </tbody>
-</table>
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+  <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="row">
+      <div class="col-lg">
+        <!-- untuk menampilkan erorr -->
+        <?php if (validation_errors()) : ?>
+          <div class="alert alert-danger" role="alert"></div>
+            <?= validation_errors(); ?>
+        <?php endif; ?>
+        <?= $this->session->flashdata('message'); ?>
+        <a href="" class="btn btn-primary mb-2" data-toggle="modal" data-target="#newSubMenuModal">Add New Submenu</a>
+      <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Menu</th>
+        <th scope="col">Url</th>
+        <th scope="col">Icon</th>
+        <th scope="col">Active</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php $i = 1; ?>
+      <?php foreach ($subMenu as $sm) : ?>
+      <tr>
+        <th scope="row"><?= $i++; ?></th>
+        <td><?= $sm['title']; ?></td>
+        <td><?= $sm['menu']; ?></td>
+        <td><?= $sm['url']; ?></td>
+        <td><?= $sm['icon']; ?></td>
+        <td><?= $sm['is_active']; ?></td>
+        <td>
+        <a href="<?= base_url(); ?>menu/editsub/<?= $sm['id']; ?>" class="badge badge-success">Edit</a>
+        <a href="<?= base_url(); ?>menu/hapus/<?= $sm['id']; ?>" class="badge badge-danger">delete</a>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 </div>
 </div>
 </div>
