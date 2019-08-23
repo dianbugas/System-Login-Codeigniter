47e64d96f9a0fb6a5f680726552cd12401694121
dianynf <dianbugas@gmail.com>
2019-08-23 23:07:16 +0700

    index user

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -14,7 +14,8 @@
   <div class="card mb-3 col-lg-8">
     <div class="row no-gutters">
       <div class="col-md-4">
-        <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail" alt="..."> //masih ada bag
+        <!-- //masih ada bag -->
+        <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
@@ -22,12 +23,12 @@
           <p class="card-text"><?= $user['email']; ?></p>
           <p class="card-text"><small class="text-muted">Terdaftar <?= date('d F Y', $user['date_created']); ?></small></p>
         </div>
-        <div class="form-file row justify-content-end">
-          <div class="col-md-9">
-            <a href="<?= base_url(); ?>user/edit/<?= $user['id']; ?>" class="btn btn-success">Edit</a>
-          </div>
-        </div>
       </div>
+    </div>
+  </div>
+  <div class="form-file row justify-content-end">
+    <div class="col-md-9">
+      <a href="<?= base_url(); ?>user/edit/<?= $user['id']; ?>" class="btn btn-success">Edit</a>
     </div>
   </div>
 </div>
