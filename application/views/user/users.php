41a1c420d2ff3b146459e90eca67ac7ce8742ad4
dianynf <dianbugas@gmail.com>
2019-08-29 11:44:17 +0700

    delete

--- application/views/user/users.php
+++ application/views/user/users.php
@@ -6,11 +6,10 @@
     <div class="col-lg">
       <!-- untuk menampilkan erorr -->
       <?php if (validation_errors()) : ?>
-        <div class="alert alert-danger" role="alert"></div>
-        <?= validation_errors(); ?>
+      <div class="alert alert-danger" role="alert"></div>
+      <?= validation_errors(); ?>
       <?php endif; ?>
       <?= $this->session->flashdata('message'); ?>
-      <a href="" class="btn btn-primary mb-2" data-toggle="modal" data-target="#newSubMenuModal">Add New User</a>
       <table class="table table-hover">
         <thead>
           <tr>
@@ -27,18 +26,18 @@
         <tbody>
           <?php $i = 1; ?>
           <?php foreach ($users as $us) : ?>
-            <tr>
-              <th scope="row"><?= $i++; ?></th>
-              <td><?= $us['name']; ?></td>
-              <td><?= $us['email']; ?></td>
-              <td><?= $us['role_id']; ?></td>
-              <td><?= $us['is_active']; ?></td>
-              <td><?= date('d F Y', $user['date_created']); ?></td>
-              <td><img src="<?= base_url('assets/img/profile/') . $us['image']; ?>" class="img-circle" alt="..." width="40" height="40"></td>
-              <td>
-                <a href="<?= base_url(); ?>admin/hapus/<?= $us['id']; ?>" class="badge badge-danger">delete</a>
-              </td>
-            </tr>
+          <tr>
+            <th scope="row"><?= $i++; ?></th>
+            <td><?= $us['name']; ?></td>
+            <td><?= $us['email']; ?></td>
+            <td><?= $us['role_id']; ?></td>
+            <td><?= $us['is_active']; ?></td>
+            <td><?= date('d F Y', $user['date_created']); ?></td>
+            <td><img src="<?= base_url('assets/img/profile/') . $us['image']; ?>" class="img-circle" alt="..." width="40" height="40"></td>
+            <td>
+              <a href="<?= base_url(); ?>admin/hapus/<?= $us['id']; ?>" class="badge badge-danger">delete</a>
+            </td>
+          </tr>
           <?php endforeach; ?>
         </tbody>
       </table>
@@ -46,52 +45,4 @@
   </div>
 </div>
 <!-- /.container-fluid -->
-</div>
-<!-- Modal -->
-<div class="modal fade" id="newSubMenuModal" tabindex="-1" role="dialog" aria-labelledby="newSubMenuModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="newSubMenuModalLabel">Add New Sub Menu</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <!-- method="post" ketika input tidak terlihat di url -->
-      <!-- action untuk mengarakan controller menu -->
-      <form action="<?php base_url('user/users'); ?>" method="post">
-        <div class="modal-body">
-          <div class="form-group">
-            <input type="text" class="form-control" id="title" name="title" placeholder="Submenu Title">
-          </div>
-          <div class="form-group  ">
-            <select name="menu_id" id="menu_id" class="form-control">
-              <option value="">Select Menu</option>
-              <?php foreach ($user as $u) : ?>
-                <option value="<?= $u['id']; ?>"><?= $u['user']; ?></option>
-              <?php endforeach; ?>
-            </select>
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" id="url" name="url" placeholder="Submenu url">
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" id="icon" name="icon" placeholder="Submenu icon">
-          </div>
-          <div class="form-group">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="1" name="is_active" id="is_active" checked>
-              <label class="form-check-label" for="is_active">
-                Active?
-              </label>
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Add</button>
-        </div>
-      </form>
-    </div>
-  </div>
 </div>
