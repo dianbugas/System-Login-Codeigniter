19f429a221247ff9392be5e9c38d95e544530d63
dianynf <dianbugas@gmail.com>
2019-08-30 17:15:52 +0700

    models submenu

--- application/views/menu/editsub.php
+++ application/views/menu/editsub.php
@@ -4,46 +4,20 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
     <div class="row">
         <div class="col-lg-8">
-            <?= form_open_multipart('menu/editsub'); ?>
-            <div class="form-group row">
-                <label for="email" class="col-sm-2 col-form-label">menu id</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="email" name="email">
+            <form action="" method='post'>
+                <input type="hidden" name="id" value="<?= $subMenu['id']; ?>">
+                <div class="form-group row">
+                    <label for="menu_id" class="col-sm-2 col-form-label">Menu Id</label>
+                    <div class="col-sm-7">
+                        <input type="text" name="menu_id" class="form-control" id="menu_id" value="<?= $menu['menu_id']; ?>">
+                        <small class="form-text text-danger"><?= form_error('menu_id'); ?></small>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="title" class="col-sm-2 col-form-label">title</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="title" name="title" value="#">
-                    <?= form_error('title', '<small class="text-danger pl-3">', '</small>'); ?>
+                <div class="form-file row justify-content-end">
+                    <div class="col-sm-10">
+                        <button type="submit" name="edit" class="btn btn-primary">Edit</button>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">url</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name" value="#">
-                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">icon</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name" value="#">
-                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">is active</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name" value="#" readonly>
-                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
-                </div>
-            </div>
-            <div class="form-file row justify-content-end">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Edit</button>
-                </div>
-            </div>
             </form>
         </div>
     </div>
